8ec8f818624b601941cbaf843c8a1f52cc438651
mountinking <xlavh25576@gmail.com>
2024-06-08 19:49:18 +0900

    day_96
    study how to use fstream libary

---
+++ day_96/practice_fstream/practice_fstream.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include < fstream>
+#include <string>
+#include < vector>
+
+using namespace std;
+
+namespace fs = std::filesystem;
+
+int main() {
+	fstream file("example.txt", ios::out | ios::in | ios::trunc);
+	if (!file) {
+		cerr << "unable to open file example.txt";
+		return 1;
+	}
+	file << "12345" << endl;
+	file << "67890" << endl;
+
+	file.seekg(6);
+	char c;
+	while (file.get(c)) {
+		cout << c;
+	}
+	file.close();
+}
